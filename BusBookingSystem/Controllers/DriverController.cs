921d7b6fac7005b1332d4684f4724c8a71658213
TasneemAhmed <https://github.com/TasneemAhmed>
2020-03-21 02:20:54 +0200

    first commit

--- BusBookingSystem/Controllers/DriverController.cs
+++ BusBookingSystem/Controllers/DriverController.cs
@@ -38,25 +38,97 @@ namespace BusBookingSystem.Controllers
             return View(driver);
         }
 
+        /*The logic to Add new Driver by admin : 
+         * mplementing the /Driver/Add URL is a two step process. When a Admin first visits the /Driver/Add URL we want to show them an HTML form that they can fill out to enter a new driver. 
+         * Then, when the admin submits the form and posts the data back to the server, we want to retrieve the posted contents and save it into our database.
+         */
         /*HTTPPost method hides information from URL and does not bind data to URL. 
         It is more secure than HttpGet method but it is slower than HttpGet. 
         It is only useful when you are passing sensitive information to the server.
         sensitive information
         */
+        
         [HttpPost] 
         public ActionResult Add(Driver createNewDriver) //mvc smart enough to bind this model to form(request) data because form data has pefix Driver
         {
-            if(ModelState.IsValid)
+            if(!ModelState.IsValid)
             {//no validtion (Data annotaion != fo
                 return View("Add", "createNewDriver");
             }
             /* when the request data to the application,
              * mvc framework will use this proprty to inititalize as parameters in action.
              */
-            db.Drivers.Add(createNewDriver); //it's only added in memory.
-            db.SaveChanges(); //it's added persitent in database.
-            return RedirectToAction("Index"); //after saveing data back to Index(list of drivers).
+             else
+            {
+                db.Drivers.Add(createNewDriver); //it's only added in memory.
+                db.SaveChanges(); //it's added persitent in database.
+                return RedirectToAction("Index"); //after saveing data back to Index(list of drivers).
+            }
+        
+           
+
         }
+
+        //An Edit link sends HttpGet request to the Edit action method of DriverController with corresponding PersonId in the query string.
+        [HttpGet]
+        public ActionResult Edit(int id) //mvc smart enough to bind this model to form(request) data because form data has pefix Driver
+        {
+            //http://localhost/driver/edit/{Id
+            //get the driver from db who has id = id the which come from form requst(data).
+            var driverFromDB = db.Drivers.SingleOrDefault(d => d.PersonId == id);
+           
+            if(driverFromDB == null)
+            {
+                return HttpNotFound();
+            }
+            /*HttpGet Edit action method will fetch student data from the database,
+             * based on the supplied Id parameter and render the Edit view with that particular Driver data.
+             */
+            return View(driverFromDB);
+           
+        }
+
+        /*The user can edit the data and click on the Save button in the Edit view.
+         * The Save button will send a HttpPOST request http://localhost/Student/Edit with the Form data collection.
+         */
+        [HttpPost]
+        public ActionResult Edit(Driver driver) 
+        {
+            /*The HttpPOST Edit action method in DriverController will finally update the data into the database 
+              and render an Index page with the refreshed data using the RedirectToAction method
+            */
+            
+            if (!ModelState.IsValid)
+            {//no validtion (Data annotaion != form data request).
+                return View("Edit", "driver");
+            }
+            //get the driver from db who has id = id the which come from form requst(data).
+            //انا هنا جيلي داتا من الفورم معمولها ايديت قبل مااعملها سيف هشوف موجود فالداتا بيز ولا ؟ موجوده هاخد الداتا بعد ماتعدلت وحطها فالداتا بيز
+            var driverFromDB = db.Drivers.SingleOrDefault(d => d.PersonId == driver.PersonId);
+
+            driverFromDB.PhoneNumber = driver.PhoneNumber;
+            driverFromDB.Salary = driver.Salary;
+            driverFromDB.UserName = driver.UserName;
+            driverFromDB.Gender = driver.Gender;
+            driverFromDB.Address = driver.Address;
+            driverFromDB.Age = driver.Age;
+            driverFromDB.EMail = driver.EMail;
+            
+            db.SaveChanges(); //it's added persitent in database.
+            return RedirectToAction("Index", "driver");
+
+        }
+
+        [HttpGet]
+        public ActionResult Delete(int id)
+        {
+            var driver = db.Drivers.SingleOrDefault(d => d.PersonId == id);
+            db.Drivers.Remove(driver);
+            db.SaveChanges();
+
+            return RedirectToAction("Index");
+        }
+
 
     }
 }
